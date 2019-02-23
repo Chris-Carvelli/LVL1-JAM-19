8bf7a1526c2d4f9ed84041e39cbba7381e5f3265
Chris-Carvelli <carve.chris@gmail.com>
2019-02-23 10:42:14 +0100

    Physic Controller (second option, necessary to handle bouncing collisions)

---
+++ LVL1-JAM-19/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerInfo {
+	public PlayerController prefab;
+	public Color color;
+	public Transform spawn;
+}
+
+public class GameManager : MonoBehaviour
+{
+	public List<PlayerInfo> players;
+	
+	// Start is called before the first frame update
+	void Start()
+    {
+		int pNum = 1;
+		foreach (PlayerInfo player in players) {
+			PlayerController pc = Instantiate(player.prefab);
+			pc.playerNumber = pNum++;
+			pc.GetComponent<SpriteRenderer>().color = player.color;
+			pc.transform.position = player.spawn.position;
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LVL1-JAM-19/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+	[Header("main")]
+	[Range(1, 2)]
+	public int playerNumber = 1;
+
+	[Header("movement")]
+	public float speed = 1;
+	//TODO if we don't use analog stricks, this will be better than rely on input settings
+	//public float accelleration = 1;
+	//public AnimationCurve accellerationCurve;
+
+	private string horAxis;
+	private string verAxis;
+
+	//public Vector2 xMargins = new Vector2(-14, 14);
+	//public Vector2 yMargins = new Vector2(-3, 32);
+
+	private Rigidbody2D body;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		body = GetComponent<Rigidbody2D>();
+
+		horAxis = $"Horizontal_{playerNumber}";
+		verAxis = $"Vertical_{playerNumber}";
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		float xVel = Input.GetAxis(horAxis) * speed;
+		float yVel = Input.GetAxis(verAxis) * speed;
+
+		body.velocity = new Vector2(xVel, yVel);
+
+		//float xCoord = transform.position.x + Input.GetAxis(horAxis) * speed;
+		//float yCoord = transform.position.y + Input.GetAxis(verAxis) * speed;
+
+		//Vector3 deltaV = new Vector3(
+		//	Mathf.Clamp(xCoord, xMargins.x, xMargins.y),
+		//	Mathf.Clamp(yCoord, yMargins.x, yMargins.y),
+		//	0
+		//	);
+
+		//transform.position = deltaV;
+	}
+}
