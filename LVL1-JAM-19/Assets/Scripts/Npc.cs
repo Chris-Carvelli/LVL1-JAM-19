dcd5ff64c86233a893ab1fb0fab59471fbee70fc
Zil85 <lzilmer@gmail.com>
2019-02-23 11:52:19 +0100

    Npc stuff

---
+++ LVL1-JAM-19/Assets/Scripts/Npc.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Npc : MonoBehaviour {
+    NavMeshAgent agent;
+    Vector3 startPosition;
+    public float roamRadius = 4;
+
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        startPosition = transform.position;
+    }
+    
+    void Update() {
+        if (!agent.hasPath && !agent.pathPending) {
+            agent.destination = RandomNavmeshLocation(roamRadius);
+        }
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius) {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += startPosition;
+        
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
+            finalPosition = hit.position;
+
+        } else if (NavMesh.SamplePosition(startPosition, out hit, radius, 1)) {
+            finalPosition = hit.position;
+
+        } else {
+            finalPosition = startPosition;
+        }
+        return finalPosition;
+    }
+}
---
+++ LVL1-JAM-19/Assets/Scripts/NpcManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcManager : MonoBehaviour {
+    public List<Npc> npcPrefabs;
+    public List<Npc> npcs;
+    public int initialNpcCount = 10;
+    
+    void Start() {
+        spawnNpcsAtRandom(initialNpcCount);
+    }
+    
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.tag == "Terrain") {
+                    spawnNpc(hit.point);
+                }
+            }
+        }
+    }
+
+    void spawnNpcsAtRandom(int totalNpcs) {
+        Vector2 levelMinCoord = new Vector2(-50, -50);
+        Vector2 levelMaxCoord = new Vector2(50, 50);
+
+        for (int i = 0; i < totalNpcs; i++) {
+            spawnNpc(new Vector3(Random.Range(levelMinCoord.x, levelMaxCoord.x), 0, Random.Range(levelMinCoord.y, levelMaxCoord.y)));
+        }
+    }
+
+    void spawnNpc(Vector3 spawnPoint) {
+        Npc npcPrefab = npcPrefabs[Random.Range(0, npcPrefabs.Count)];
+        Npc newNpc = Instantiate(npcPrefab, spawnPoint + Vector3.up, npcPrefab.transform.rotation);
+        npcs.Add(newNpc);
+    }
+}
